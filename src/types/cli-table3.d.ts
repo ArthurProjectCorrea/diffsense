33d293d46c55d375a1b7ffd1e613a2a61a044464
arthurdepaulacorrea@hotmail.com <arthurdepaulacorrea@hotmail.com>
2025-08-19 10:24:13 -0400

    feat: adiciona nova funcionalidade

---
+++ src/types/cli-table3.d.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// Type declaration for cli-table3
+declare module 'cli-table3' {
+  const Table: any;
+  export default Table;
+}
---
+++ src/types/custom.d.ts
@@ -0,0 +1,4 @@
+// Ambient declarations for modules without types
+declare module 'chalk';
+declare module 'boxen';
+declare module 'cli-table3';
